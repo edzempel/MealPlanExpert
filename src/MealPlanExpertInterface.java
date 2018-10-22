4f0e69761f680628c91f5eac9ae2aaa6decd9488
edzempel <edward.zempel@gmail.com>
2018-10-22 17:21:44 -0500

    add MealPlanExpertInterface add .gitignore ignore all IDE settings files

--- src/MealPlanExpertInterface.java
+++ src/MealPlanExpertInterface.java
@@ -1,11 +1,15 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
-import java.awt.*;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.TextField;
 
 public class MealPlanExpertInterface  extends Application {
+
+/*
+UI elements must be declared outside of start() so they are
+not out of scope for the embedded classes
+*/
 
     Stage window;
 
@@ -16,18 +20,39 @@ public class MealPlanExpertInterface  extends Application {
     //Panes
     VBox profilePane;
 
-    //profileScene tiems
+    //profileScene items
     TextField profileName;
 
     public static void main(String[] args) {
+
         Application.launch(args);
     }
 
     @Override
     public void start(Stage primaryStage){
 
-        //Profile pane
+        window = primaryStage;
+        primaryStage.setTitle("Meal Plan Expert");
+
+        //Declare and instantiate instance profile pane items
+        profileName = new TextField("profile name");
+
+        // populate the profile pane
         profilePane = new VBox();
+        profilePane.getChildren().add(profileName);
+
+        // instantiate the profile scene with the pane and dimensions
+        profileScene = new Scene(profilePane, 300, 400);
+
+
+        //populate stage with scene, use this syntax for switching scenes
+        //window.setScene(profileScene);
+
+        // initial scene
+        primaryStage.setScene(profileScene);
+        // call .show() after scene is set, this displays the interface
+        primaryStage.show();
 
     }
+
 }
